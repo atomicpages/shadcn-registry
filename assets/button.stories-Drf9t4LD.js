e1790c2254b76b83b9469f95936e5501c9eb46ba
atomicpages <atomicpages@users.noreply.github.com>
2025-03-31 03:57:45 +0000

    Deploying to gh-pages from @ atomicpages/shadcn-registry@e12b251deb88b1f78f1e76f24aca28b0ae4588aa 🚀

---
+++ assets/button.stories-Drf9t4LD.js
@@ -0,0 +1,28 @@
+import{j as b}from"./jsx-runtime-BjG_zV1W.js";import{B as I}from"./button-DX86pcIG.js";import{c as T}from"./createLucideIcon-DhchgwkM.js";import"./index-CE0XXScK.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-Dv1nZR32.js";import"./index-qVV5lYCa.js";import"./utils-eOuC519h.js";/**
+ * @license lucide-react v0.460.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=T("House",[["path",{d:"M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8",key:"5wwlr5"}],["path",{d:"M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z",key:"1d0kgt"}]]),r=["default","destructive","ghost","link","outline","secondary"],H={title:"Components/Button",component:I,tags:["autodocs"],argTypes:{variant:{description:"The visual appearance of the button is controlled by variants.",options:r,control:{type:"select"},table:{defaultValue:{summary:"primary"},type:{summary:"string",detail:`Possible values are
+
+${r.join(`
+`)}`}}},loading:{control:{type:"boolean"},description:"Set true to enable the button loader.",table:{type:{summary:"boolean"},defaultValue:{summary:"false"}}},disabled:{control:{type:"boolean"},description:"Set true to disable the button.",table:{type:{summary:"boolean"},defaultValue:{summary:"false"}}},auto:{control:{type:"boolean"},description:"Set true to make the button 100% width.",table:{type:{summary:"boolean"},defaultValue:{summary:"false"}}},leftIcon:{control:{type:"text",disable:!0},description:"The left icon of the button.",table:{type:{summary:"React.ReactNode"},defaultValue:{summary:"undefined"}}},rightIcon:{control:{type:"text",disable:!0},description:"The right icon of the button.",table:{type:{summary:"React.ReactNode"},defaultValue:{summary:"undefined"}}}}},e={args:{children:"Testing"}},t={args:{children:"Testing",loading:!0}},o={args:{children:"Testing",leftIcon:b.jsx(f,{})}},a={args:{children:"Testing",rightIcon:b.jsx(f,{})}};var n,s,c;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    children: "Testing"
+  }
+}`,...(c=(s=e.parameters)==null?void 0:s.docs)==null?void 0:c.source}}};var i,l,u;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    children: "Testing",
+    loading: true
+  }
+}`,...(u=(l=t.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var d,m,p;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    children: "Testing",
+    leftIcon: <HouseIcon />
+  }
+}`,...(p=(m=o.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var g,y,h;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    children: "Testing",
+    rightIcon: <HouseIcon />
+  }
+}`,...(h=(y=a.parameters)==null?void 0:y.docs)==null?void 0:h.source}}};const L=["SampleButton","LoadingButton","LeftIconButton","RightIconButton"];export{o as LeftIconButton,t as LoadingButton,a as RightIconButton,e as SampleButton,L as __namedExportsOrder,H as default};
